873e088390862e53b93975099e98b07b2c43acc4
pulindu <pulindujanith@gmail.com>
2023-06-09 18:25:43 +0530

    remove testing until issues are resolved

--- web/src/test/java/com/snapped/web/TestWebApplication.java
+++ web/src/test/java/com/snapped/web/TestWebApplication.java
@@ -1,22 +1,10 @@
 package com.snapped.web;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
+
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestWebApplication {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>("postgres:latest");
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.from(WebApplication::main).with(TestWebApplication.class).run(args);
-	}
 
 }
